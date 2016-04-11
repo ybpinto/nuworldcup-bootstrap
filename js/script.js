cf8eca489a893f552e9de8e867aa2633b6de61a7
Yoni Pinto <yoni.b.pinto@gmail.com>
2016-04-11 00:57:10 -0500

    scroll down to group fixture when clicking group, enabled smooth scrolling js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
 
-	// // Smooth scrolling code
-	// $('a[href^="#"]').on('click',function (e) {
-	//     e.preventDefault();
+	// Smooth scrolling code
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
 
-	//     var target = this.hash;
-	//     var $target = $(target);
+	    var target = this.hash;
+	    var $target = $(target);
 
-	//     $('html, body').stop().animate({
-	//         'scrollTop': $target.offset().top
-	//     }, 600, 'swing');
-	// });
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 600, 'swing');
+	});
 
 });
